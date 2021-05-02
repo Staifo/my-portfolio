2998a337bbaed2a60dbb051517999c844a060953
Istayfo <i.ergun@gmx.de>
2021-05-02 12:19:38 +0200

    test

--- src/info.js
+++ src/info.js
@@ -105,19 +105,17 @@ const Info = () => {
           This project was created by me in individual work in around 2 weeks.
           The idea for my final project was to create a platform just for Junior
           Coders, where they can present themselves and their skills via short
-          video ( which can be recorded directly on this platform during the
-          registration process via build in component with access to the local
-          webcam ) CV and other sources to prospective employers and furthermore
+          video, CV and other sources to prospective employers and furthermore
           to give employers an opportunity to just look for Junior Coders to
-          give them a better chance to find a way into the industry. Once you go
+          give them a better chance to find a way into the industry. Videos can
+          be recorded directly on this platform during the registration process
+          via build in component with access to the local webcam. Once you go
           through the registration process a new user card and the appropriate
-          personal space is created automatically. Not all functions have been
-          implemented yet in order to keep it easily accessible but functions
-          have been prepared in the backend.The current videos are improvised.
+          personal space is created automatically. The current videos are improvised.
           The persons in the videos are fellow students. I used ReactJS, NodeJS,
           Express, Material UI, Bootstrap, MongoDB ... to create this project
-          and work is still going on. 
-          On iOS: Videos dont't play on iOS since the videos are recorded and saved in WebM format.
+          and work is still going on. On iOS: Videos dont't play on iOS since
+          the videos are recorded and saved in WebM format.
         </CardContent>
       </Collapse>
     </Card>
